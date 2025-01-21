516190ee12837dbae0780b602e04004a415496cb
Fred <fcf.camposx@gmail.com>
2025-01-21 08:51:29 -0400

    work changes

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -1,4 +1,10 @@
 return {
   "tpope/vim-sleuth",
   "nvim-lua/plenary.nvim",
+  {
+    "folke/ts-comments.nvim",
+    opts = {},
+    event = "VeryLazy",
+    enabled = vim.fn.has("nvim-0.10.0") == 1,
+  }
 }
